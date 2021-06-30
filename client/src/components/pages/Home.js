a64b64bfc98c7923833d52f5b2a9e6f616257473
Vicky Ungemach <vicky.ungemach@gmail.com>
2021-06-30 07:40:03 -0700

    lists

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -18,6 +18,10 @@ const Home = () => {
                 <Badge variant="pill" badge="new">Pill Badge</Badge>
                 <Badge variant="button" badge="35">Button Badge</Badge>
 
+            { /* ===================================
+               Cards
+            =================================== */ }
+
                 <div className="row">
                     <div className="col-4">
 
@@ -95,6 +99,47 @@ const Home = () => {
 
                 </div>
             </div>
+
+            <div className="row">
+                <div className="col-5">
+
+                    {/* Basic list */}
+                    <h4 className="mb-5">Basic List</h4>
+                    <ul className="list-group">
+                        <li className="list-group-item">Featured</li>
+                        <li className="list-group-item">Cras justo odio</li>
+                        <li className="list-group-item">Dapibus ac facilisis in</li>
+                        <li className="list-group-item">Vestibulum at eros</li>
+                    </ul>
+
+                </div>
+            </div>
+
+            { /* ===================================
+               Lists
+            =================================== */ }
+            <div className="row">
+                <div className="col">
+                    <ul className="list-group list-group-horizontal">
+                        <li className="list-group-item">Featured</li>
+                        <li className="list-group-item">Cras justo odio</li>
+                        <li className="list-group-item">Dapibus ac facilisis in</li>
+                        <li className="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <ul className="list-group">
+                        <li className="list-group-item">Featured </li>
+                        <li className="list-group-item">Cras justo odio <span className="badge badge-pill">16</span> </li>
+                        <li className="list-group-item">Dapibus ac facilisis in <span className="badge badge-pill">22</span></li>
+                        <li className="list-group-item">Vestibulum at eros<span className="badge badge-pill">12</span> </li>
+                    </ul>
+                </div>
+            </div>
+
 
             </div>
         </>
