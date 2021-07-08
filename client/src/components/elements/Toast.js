547666324e694ccca661ea86301760d708a74738
Vicky Ungemach <vicky.ungemach@gmail.com>
2021-07-08 07:27:17 -0700

    finishing toast

--- client/src/components/elements/Toast.js
+++ client/src/components/elements/Toast.js
@@ -1,20 +1,70 @@
 import React from 'react';
 
 const Toast = () => {
+
+    function showToast() {
+        const toast = document.getElementById('toast');
+        toast.classList.add('show');
+
+        setTimeout(() => {
+            toast.classList.remove('show');
+        }, 3000);
+    }
+
+    function hideToast() {
+        document.getElementById('toast').classList.remove('show');
+    }
+
     return (
-        <div className="mb-10">
-            <div class="toast">
-                <div class="toast-header">
-                    <div className="toast-img"></div>
-                    <strong className="toast-title">Toast</strong>
-                    <small>11 mins ago</small>
-                    <button className="toast-close"><span>&times;</span></button>
-                </div>
-                <div class="toast-body">
-                    Hello, world! This is a toast message.
+        <>
+            <div className="mb-3">
+                <div class="toast">
+                    <div class="toast-header">
+                        <div className="toast-img"></div>
+                        <strong className="toast-title">Toast</strong>
+                        <small>11 mins ago</small>
+                        <button className="toast-close"><span>&times;</span></button>
+                    </div>
+                    <div class="toast-body">
+                        Hello, world! This is a toast message.
+                    </div>
                 </div>
             </div>
-        </div>
+
+            <div className="mb-10">
+                <button className='btn btn-primary' onClick={showToast}>
+                    Show toast
+                </button>
+            </div>
+
+
+            {/* Live toast */}
+
+            <div className="toast-container" id="toast">
+                <div class="toast">
+                    <div class="toast-header">
+                        <div className="toast-img"></div>
+                        <strong className="toast-title">Toast</strong>
+                        <small>11 mins ago</small>
+                        <button className="toast-close" onClick={hideToast}><span>&times;</span></button>
+                    </div>
+                    <div class="toast-body">
+                        Hello, world! This is a toast message.
+                    </div>
+                </div>
+                <div class="toast">
+                    <div class="toast-header">
+                        <div className="toast-img"></div>
+                        <strong className="toast-title">Toast</strong>
+                        <small>11 mins ago</small>
+                        <button className="toast-close" onClick={hideToast}><span>&times;</span></button>
+                    </div>
+                    <div class="toast-body">
+                        Hello, world! This is a toast message.
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -20,7 +20,6 @@ const Home = () => {
             <div className="container">
                 <Toast />
                 <Alert />
-                <Button />
                 <Badge />
                 <Card />
                 <Collapse />
