18ce049809e0dfed6225c136db89c992e72907c5
Vicky Ungemach <vicky.ungemach@gmail.com>
2021-08-03 05:35:45 -0700

    refactor dropdown

--- client/src/components/elements/Dropdown.js
+++ client/src/components/elements/Dropdown.js
@@ -1,19 +1,21 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = () => {
 
-    function toggleDropdown(e) {
-        document.querySelector(`[data-dropdown=${e.target.id}]`).classList.toggle('show');
+    const [dropdown, setDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdown(!dropdown);
     }
 
     return (
         <section id="dropdown">
             <div className="dropdown">
-                <button className="btn btn-primary btn-dropdown" id="dropdown1" onClick={toggleDropdown}>
+                <button className="btn btn-primary btn-dropdown" onClick={toggleDropdown}>
                     Dropdown button
                 </button>
-                <ul className="dropdown-menu" data-dropdown="dropdown1">
+                <ul className={`dropdown-menu ${dropdown ? 'show' : ''}`}>
                     <li><a className="dropdown-item" href="#">Action</a></li>
                     <li><a className="dropdown-item" href="#">Another action</a></li>
                     <li><a className="dropdown-item" href="#">Something else here</a></li>
